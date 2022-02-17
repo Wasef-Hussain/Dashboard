90cb99f1379514ddbbb06d5fc8769256ebe3a86a
Sulaiman <sulaiman.amer@gmail.com>
2022-02-17 22:47:16 +0500

    added upper div

--- src/component/Dashboard/index.js
+++ src/component/Dashboard/index.js
@@ -8,15 +8,23 @@ function Dashboard() {
         <div className='row'>
             <div className='leftarea p-3'>
                 <div className='chartareaa col-12' >
+                    <div className='upper-div'>
+                        <div className='workSummaryL' >
 
+                        </div>
+
+                        <div className='playButtonR' >
+
+                        </div>
+                 </div>
                 </div>
                 <div className='tableareaa col-12' >
 
                 </div>
             </div>
-            <div className='rightarea'>
+                <div className='rightarea'>
 
-            </div>
+                </div>
         </div>
     )
 }
