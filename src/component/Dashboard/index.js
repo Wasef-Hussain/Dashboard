4073a87847a377c35aaa2a8fc37d3797ac7635af
Sulaiman <sulaiman.amer@gmail.com>
2022-02-18 03:13:19 +0500

    right bars

--- src/component/Dashboard/index.js
+++ src/component/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { facirclecheck, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { facirclecheck, faCheckCircle, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 import { MdSmartDisplay } from "react-icons/md";
@@ -32,14 +32,34 @@ function Dashboard() {
                 </div>
             </div>
             <div className='rightarea' style={{ paddingLeft: '1vh' }}>
-                <div className='col-auto' style={{ width: '52vh', height: '25vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
+                <div className='col-auto' style={{ width: '52vh', height: '18vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
+
+                <div className='rightBarL'>
+                <FontAwesomeIcon icon={faCheckCircle} style={{ color: "#262C44", height: "2vh", marginLeft:"0.4rem" }} />
+                <h6 className=' newfont'>Bitcoin</h6> <h6 className=' newfont'>$67,205.10</h6>
+                
+                <div className='arrowup'>
+                <FontAwesomeIcon icon={faArrowUp}/> <h6 className=' newfont'>37.8% this week</h6>
+                </div>
+                </div>
+
+                <div className='rightBarR'>
+
 
                 </div>
-                <div className='col-auto' style={{ width: '52vh', height: '25vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
+                </div>
+                <div className='col-auto' style={{ width: '52vh', height: '18vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
+
+
+
+                </div>
+                <div className='col-auto' style={{ width: '52vh', height: '18vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
+
+
 
                 </div>
                 <div className='col-auto' style={{ width: '52vh', height: '38vh', borderRadius: '10px', background: '#fff', marginTop: '2vh', backgroundColor: '#10131E', border: '1px solid #1B2031' }}>
-
+                <h6 className=' newfont' style={{fontFamily:"", color:"#fff"}} >Daily Earning</h6> <FontAwesomeIcon icon={faCheckCircle} style={{ color: "#262C44", height: "2vh", marginLeft:"0.4rem" }} />
                 </div>
             </div>
         </div>
